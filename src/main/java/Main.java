5ba2c0d4f8dd325fffb452dedc35161101c5052d
Beachlessfawn2 <kenzie.coddens.y1678@student.hogent.be>
2020-05-20 15:45:24 +0200

    rvfdc

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-  public static void main(String[] args) {
-        MessageUtil mu = new MessageUtil("Idioten zijn er overal...");
-        mu.printMessage();
-        mu.salutationMessage();
-  }
-}
